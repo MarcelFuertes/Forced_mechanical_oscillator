4aefc5d36b2fc308b54ee6c12023209df24d2a9c
MarcelFuertes <marcel.fuertes@estudiantat.upc.edu>
2022-11-22 20:10:22 +0100

    update rk4

--- c_files/RK4.c
+++ c_files/RK4.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 #include<math.h>
 #define PI 3.141592653589793
-#define N 300
+//#define N 300
 #define B 2.
 #define Wo 10.
 #define M 0.1
 #define Fo 1.
-
+#define cte 0.2
 
 double g(double v);
 double f(double x, double v, double t);
@@ -20,23 +20,26 @@ void error(double h, double t[N], double x[N],double v[N], double tn);
 int main(){
     FILE *fp;
     FILE *fp2;
-    fp=fopen("RK4-points.txt","w");
-    fp2=fopen("RK4Errors.txt","w");
-    double t[N],x[N],v[N],tn,h;
-    y[0]=1;
-    t[0]=0;
-    //printf("Comencem la iteració desde t0=0, fins quin t_n vols arribar?:\n");
-    //scanf("%lf",&tn);
-    tn=10*PI/Wo;
-    printf("Introdueix el pas h:\n");
+    fp=fopen("RK4-points.txt","w"); //we create the txt file that contains the points of the RK4 algorithm
+    fp2=fopen("RK4Errors.txt","w"); //we create the txt file that contains the exact errors.
+    printf("Introduce the value for h:\n");
     scanf("%lf",&h);
+	double tn = 10*PI/Wo;
+	int N;
+	double value=tn/h;
+	N = (int)value+1;
+    double t[N],x[N],v[N],tn,h;
+    x[0]=0.;
+	v[0]=1.;
+    t[0]=0.;
+
     rk_4(h,t,x,v,tn);
     error(h,t,x,v,tn);
     int i;
     for(i=0;i<=(tn/h)+h;i++){
         fprintf(fp,"%.16G  \t %.16G \n",t[i],x[i]);
     }
-    for(i=0;i<=(tn/h)+h;i++){
+    for(i=0;i<=(tn/h)+h;i++){ //this should be in the error function
         fprintf(fp2,"%.16G  \t %.16G \n ",t[i], fabs(solExacta(t[i])-x[i]));
     }
     fclose(fp);
@@ -50,12 +53,23 @@ double g(double v){
 }
 
 double f(double x, double v, double t){
-    double w=0.2*Wo;
+    double w=cte*Wo;
     return -(B/M)*v-Wo*Wo*x+(Fo/M)*cos(w*t);
 }
 
-double solExacta(double t){ //pendent de canviar
-    return 1000/(1+999*exp(2*t));
+double solExacta(double t){
+	double lambda1,lambda2,alpha,theta;
+	double w=cte*Wo;
+	lambda1=(B+sqrt(B*B-4.*M*M*Wo*Wo))/(2.*M);
+	lambda2=(B-sqrt(B*B-4.*M*M*Wo*Wo))/(2.*M);
+	alpha=Fo/sqrt(M*M*(Wo*Wo-w*w)*(Wo*Wo-w*w)+B*B*w*w);
+	theta=atan((B*w)/(M*(Wo*Wo-w*w)));
+		
+	double c1,c2;
+	c1=(alpha*lambda2*cos(theta)+alpha*w*sin(theta)-1.)/(lambda1-lambda2);
+	c2=(alpha*lambda1*cos(theta)+alpha*w*sin(theta)-1.)/(lambda2-lambda1);
+
+    return c1*exp(-t*lambda1)+c2*exp(-t*lambda2)+alpha*cos(w*t-theta);
 }
 
 void rk_4(double h, double t[N], double x[N], double v[N], double tn){
@@ -86,7 +100,7 @@ void rk_4(double h, double t[N], double x[N], double v[N], double tn){
     }
 }
 
-void error(double h, double t[N], double x[N], double v[N], double tn){
+void error(double h, double t[N], double x[N], double v[N], double tn){//this should save the points in a txt file
     int i=0;
     double exacta[N];
     printf("Iteration \t t_i \t\t SolExacta \t y_i \t\t error \t \n ");
